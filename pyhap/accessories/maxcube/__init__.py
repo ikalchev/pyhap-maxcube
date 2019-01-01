91fb56a4c7470af7c8da828a402ebdc9f68f118d
Ivan Kalchev <ivan.kalchev@icloud.com>
2019-01-01 15:13:02 +0200

    Regularly update MAX state, set target T

--- pyhap/accessories/maxcube/__init__.py
+++ pyhap/accessories/maxcube/__init__.py
@@ -1,16 +1,17 @@
 """Defines bridges to MAX! devices.
 
 - Thermostat ``Accessory`` interfacing with a MAX! thermostat.
-- MaxBridge, which discovers and Bridges MAX! devices.
+- MaxBridge, which discovers and bridges MAX! devices.
 """
 import logging
+import socket
 
 from maxcube.connection import MaxCubeConnection
 from maxcube.cube import MaxCube
 from maxcube.device import (MAX_THERMOSTAT,
                             MAX_DEVICE_MODE_AUTOMATIC)
 
-from pyhap import accessory
+from pyhap import accessory, characteristic
 from pyhap.const import CATEGORY_THERMOSTAT
 from pyhap.util import event_wait
 
@@ -29,6 +30,8 @@ class Thermostat(accessory.Accessory):
     model = THERMOSTAT_MODEL
     """The model of the MAX! line, e.g. Cube or Thermostat"""
 
+    common_properties = {characteristic.PROP_MIN_STEP: 0.5}
+
     def __init__(self, cube, device, *args, **kwargs):
         """Create a Thermostat for the given ``device``.
 
@@ -44,6 +47,7 @@ class Thermostat(accessory.Accessory):
 
         self.cube = cube
         self.device = device
+        self.pending_updates = False
 
         # Set the AccessoryInformation service values from the device
         self.set_info_service(serial_number=device.serial,
@@ -80,25 +84,28 @@ class Thermostat(accessory.Accessory):
         service.configure_char(
             'CurrentTemperature',
             value=device.actual_temperature,
-            getter_callback=lambda: self.device.actual_temperature or 0)
+            getter_callback=lambda: self.device.actual_temperature or 0,
+            properties=self.common_properties
+        )
 
         service.configure_char(
             'TargetTemperature',
             value=device.target_temperature,
-            setter_callback=lambda x: self.cube.set_target_temperature(self.device, x),
-            getter_callback=lambda: self.device.target_temperature or 0
+            setter_callback=self._set_target_temperature,
+            getter_callback=lambda: self.device.target_temperature or 0,
+            properties=self.common_properties
         )
 
         service.configure_char(
             'CurrentHeatingCoolingState',
-            value=self._state(),
-            getter_callback=self._state,
+            value=self._current_state(),
+            getter_callback=self._current_state,
         )
 
         service.configure_char(
             'TargetHeatingCoolingState',
-            value=self._state(),
-            getter_callback=self._state,
+            value=self._target_state(),
+            getter_callback=self._target_state,
         )
 
         service.configure_char(
@@ -106,13 +113,55 @@ class Thermostat(accessory.Accessory):
             value=0,  # Celsius
         )
 
-    def _state(self):
+    def _target_state(self):
         """Map device.mode to HAP heating state."""
-        if self.device.mode == MAX_DEVICE_MODE_AUTOMATIC:
-            mode = 3  # Automatic
+        if (self.device.mode or MAX_DEVICE_MODE_AUTOMATIC) == MAX_DEVICE_MODE_AUTOMATIC:
+            return 3  # Automatic
         else:
-            mode = 0  # Off
-        return mode
+            return 0  # Off
+
+    def _current_state(self):
+        """ """
+        if self.device.actual_temperature is None \
+            or self.device.target_temperature is None:
+            return 0
+        if self.device.actual_temperature > self.device.target_temperature:
+            return 0  # Off
+        else:
+            return 1  # Heat
+
+    def _set_target_temperature(self, value):
+        self.driver.add_job(self._do_set_target_temperature(value))
+
+    async def _do_set_target_temperature(self, value):
+        """ """
+        while True:
+            try:
+                await self.driver.async_add_job(
+                    self.cube.set_target_temperature, self.device, value)
+            except socket.timeout:
+                logging.warning('Timeout setting target temperature.')
+            else:
+                logging.debug('Successfully set target temperature to %s.', value)
+                break
+
+    def update(self):
+        """Update the characteristics with the values in ``self.device``.
+        """
+        service = self.get_service('Thermostat')
+        service.get_characteristic('CurrentTemperature')\
+               .set_value(self.device.actual_temperature or 0)
+
+        service.get_characteristic('TargetTemperature')\
+               .set_value(self.device.target_temperature or 0)
+
+        service.get_characteristic('CurrentHeatingCoolingState')\
+               .set_value(self._current_state())
+
+        service.get_characteristic('TargetHeatingCoolingState')\
+               .set_value(self._target_state())
+
+
 
 
 class MaxBridge(accessory.Bridge):
@@ -120,7 +169,7 @@ class MaxBridge(accessory.Bridge):
     as HAP accessories.
     """
 
-    def __init__(self, address, port, *args, **kwargs):
+    def __init__(self, address, port, *args, update_interval=60, **kwargs):
         """Create a bridge to the MAX! Cube running on the given address.
 
         After initialisation, it will attempt to discover all Thermostats and create
@@ -134,10 +183,16 @@ class MaxBridge(accessory.Bridge):
         self.address = address
         self.port = port
         self.cube = None
+        self.update_interval = 60
         self.discover()
 
     def discover(self):
-        self.cube = MaxCube(MaxCubeConnection(self.address, self.port))
+        try:
+            self.cube = MaxCube(MaxCubeConnection(self.address, self.port))
+        except socket.timeout:
+            logging.error('Could not connect to MAX! cube when setting up the bridge. '
+                'Make sure nothing else is connected to the MAX! cube at start up and '
+                'try again.')
         for device in self.cube.devices:
             if device.type != MAX_THERMOSTAT:
                 logging.info('Discovered non-thermostat')
@@ -150,10 +205,17 @@ class MaxBridge(accessory.Bridge):
         :see: ``update``
         """
         await super().run()
-        await self.update()
+        while not await event_wait(self.driver.aio_stop_event, self.update_interval):
+            logging.debug('Updating MAX! info.')
+            await self.update()
 
     async def update(self):
-        """Fetch the data from MAX! cube every 30 seconds."""
-        while not await event_wait(self.driver.aio_stop_event, 30):
-            logging.debug('Updating MAX! info.')
+        """Fetch data from MAX! cube."""
+        try:
             await self.driver.async_add_job(self.cube.update)
+            for acc in self.accessories.values():
+                acc.update()
+        except socket.timeout as timeout_error:
+            logging.warning('Could not update MAX! info, because of exception: %s',
+                            timeout_error)
+
